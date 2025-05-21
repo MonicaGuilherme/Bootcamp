cbf52430667feed717d6b2b5f273ba1975973219
MonicaGuilherme <mfguilherme@gmail.com>
2025-05-21 20:11:55 +0100

    delete API to avoid problems with GitHob

--- CatsWorld/js/service/catService.js
+++ CatsWorld/js/service/catService.js
@@ -1,23 +1,23 @@
-const API_KEY = 'live_fc13trzsco3nqABrLmPuQk8dj2UBIsj003fsYUD66iGIamCm8ANMrEi77m8gJYBv';
+const API_KEY = /* API */;
 
 const headers = {
   'x-api-key': API_KEY
 };
 
 async function getFacts() {
-  const response = await fetch('https://meowfacts.herokuapp.com/?count=5');
+  const response = await fetch(/* URL */);
   const data = await response.json();
   return data.data;
 }
 
 async function getCatImages() {
-  const response = await fetch('https://api.thecatapi.com/v1/images/search?limit=5', { headers });
+  const response = await fetch(/* URL */, { headers });
   const data = await response.json();
   return data.map(img => img.url);
 }
 
 async function getCats() {
-  const response = await fetch('https://api.thecatapi.com/v1/breeds', { headers });
+  const response = await fetch(/* URL */, { headers });
   const data = await response.json();
   return data;
 }
